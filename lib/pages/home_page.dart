dae1bfcf38b63a5451895ee1f7620154b4f9cb3f
tiongjjyi <tiongjjyi@gmail.com>
2024-03-21 22:14:17 +0800

    add bottom nav bar, shop page and cart page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:ios_app/components/bottom_nav_bar.dart';
+import 'package:ios_app/pages/cart_page.dart';
+import 'package:ios_app/pages/shop_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //to control bottom nav bar
+  int _selectedIndex = 0;
+
+  //will update selected item
+  //when user tap on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //pages to display
+  final List<Widget> _pages = [
+    //shop page
+    const ShopPage(),
+
+    //cart page
+    const CartPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
   }
 }
